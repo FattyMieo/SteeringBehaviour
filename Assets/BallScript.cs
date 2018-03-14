e4384a23be2994f59b1dab12ac6587afdf3c7757
FattyMieo <fong2009@live.com.my>
2018-03-14 19:44:19 +0800

    Finish Gravity Ball

---
+++ Assets/BallScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallScript : MonoBehaviour
+{
+	public Transform gravField;
+
+	public float mass = 1.0f;
+	public Vector2 velocity = Vector2.zero;
+	public Vector2 acceleration = Vector2.zero;
+
+	void Update()
+	{
+		if(Input.GetMouseButton(0)) //Resets the velocity & acceleration
+		{
+			velocity = Vector2.zero;
+			acceleration = Vector2.zero;
+		}
+		if(Input.GetMouseButton(1)) //Resets the position to the gravity field
+		{
+			transform.position = gravField.position;
+		}
+
+		Vector2 dir = gravField.position - transform.position;
+		AddForce2D(dir.magnitude, dir.normalized);
+	}
+
+	void FixedUpdate ()
+	{
+		velocity += acceleration;
+		transform.position += new Vector3(velocity.x, velocity.y, 0.0f);
+	}
+
+	public void AddForce2D(float magnitude, Vector2 direction)
+	{
+		//Make sure the direction is normalized
+		direction.Normalize();
+
+		//F = ma
+		acceleration = direction * magnitude / mass;
+	}
+}
---
+++ Assets/GravityFieldControl.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityFieldControl : MonoBehaviour
+{
+	void Update ()
+	{
+		Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		pos.z = 0.0f;
+		transform.position = pos;
+	}
+}
