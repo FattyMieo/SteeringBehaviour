3c1f8b6f71c407648a90389b94dd1ca469bcac18
FattyMieo <fong2009@live.com.my>
2018-03-21 19:19:55 +0800

    More Settings & Repelling

--- Assets/Scripts/BallScript.cs
+++ Assets/Scripts/BallScript.cs
@@ -4,11 +4,27 @@ using UnityEngine;
 
 public class BallScript : MonoBehaviour
 {
-	public Transform gravField;
-
+	[Header("Physics")]
 	public float mass = 1.0f;
 	public Vector2 velocity = Vector2.zero;
 	public Vector2 acceleration = Vector2.zero;
+
+	[Header("Repel Settings")]
+	public float repelRadius = 0.5f;
+	public List<Transform> nearbyBalls;
+	private List<Vector2> externalForces = new List<Vector2>();
+	public float repelAmplification = 5.0f;
+
+	[Header("Other Settings")]
+	public bool ignoreMass = false;
+	public bool useFixedAcceleration = false;
+	public float fixedAcceleration = 0.1f;
+	public bool useCappedSpeed = false;
+	public float maxSpeed = 3.0f;
+
+	[Header("Extra Stuff")]
+	public bool useSpecialEffect = false;
+	public float minSize = 0.0001f;
 
 	void Update()
 	{
@@ -19,16 +35,46 @@ public class BallScript : MonoBehaviour
 		}
 		if(Input.GetMouseButton(1)) //Resets the position to the gravity field
 		{
-			transform.position = gravField.position;
+			transform.position = SpawnManager.instance.gravField.position;
 		}
 
-		Vector2 dir = gravField.position - transform.position;
-		AddForce2D(dir.magnitude, dir.normalized);
+		Vector2 dir = SpawnManager.instance.gravField.position - transform.position;
+
+		CheckNearbyBalls();
+		Vector2 externalForce = Vector2.zero;
+		for(int i = 0; i < externalForces.Count; i++)
+		{
+			externalForce += externalForces[i] * repelAmplification;
+		}
+
+		float totalMag = 0.0f;
+
+		if(useFixedAcceleration)
+			totalMag = fixedAcceleration;
+		else
+			totalMag = (dir + externalForce).magnitude;
+		
+		AddForce2D(totalMag, (dir + externalForce).normalized);
+
+		//Extra stuff
+		if(useSpecialEffect)
+		{
+			transform.localScale = new Vector3(acceleration.sqrMagnitude + minSize, acceleration.sqrMagnitude + minSize, 1.0f) * mass;
+		}
 	}
 
 	void FixedUpdate ()
 	{
 		velocity += acceleration;
+
+		if(useCappedSpeed)
+		{
+			if(velocity.sqrMagnitude > maxSpeed * maxSpeed)
+			{
+				velocity = velocity.normalized * maxSpeed;
+			}
+		}
+		
 		transform.position += new Vector3(velocity.x, velocity.y, 0.0f);
 	}
 
@@ -38,6 +84,29 @@ public class BallScript : MonoBehaviour
 		direction.Normalize();
 
 		//F = ma
-		acceleration = direction * magnitude / mass;
+		acceleration = direction * magnitude;
+		if(!ignoreMass) acceleration /= mass;
+	}
+
+	public void CheckNearbyBalls()
+	{
+		Transform[] ballList = SpawnManager.instance.ballList.ToArray();
+
+		externalForces.Clear();
+		//Debug
+		nearbyBalls.Clear();
+
+		for(int i = 0; i < ballList.Length; i++)
+		{
+			if(ballList[i] == this.transform) continue;
+
+			Vector3 dir = transform.position - ballList[i].position;
+			if(dir.sqrMagnitude <= repelRadius * repelRadius)
+			{
+				externalForces.Add((Vector2)dir);
+				//Debug
+				nearbyBalls.Add(ballList[i]);
+			}
+		}
 	}
 }
---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+	private static SpawnManager _instance;
+
+	public static SpawnManager instance
+	{
+		get { return _instance; }
+	}
+
+	[Header("Settings")]
+	public int amount = 50;
+	public Vector2 minMaxMass = new Vector2(250.0f, 2000.0f);
+
+	public List<Transform> ballList;
+
+	[Header("Prefabs")]
+	public GameObject ballPrefab;
+
+	[Header("Connections")]
+	public Transform gravField;
+
+	void Awake ()
+	{
+		if(_instance == null) _instance = this;
+		else if(_instance != this) Destroy(gameObject);
+	}
+
+	void Start ()
+	{
+		for(int i = 0; i < amount; i++)
+		{
+			Vector3 randVec = new Vector3(Random.Range(0.0f, Screen.width), Random.Range(0.0f, Screen.height), 0.0f);
+			randVec = Camera.main.ScreenToWorldPoint(randVec);
+			randVec.z = 0.0f;
+
+			GameObject newBall = Instantiate(ballPrefab, randVec, Quaternion.identity, this.transform);
+			newBall.name = ballPrefab.name + " " + i.ToString("000");
+			newBall.GetComponent<BallScript>().mass = Random.Range(minMaxMass.x, minMaxMass.y);
+			ballList.Add(newBall.transform);
+		}
+	}
+}
